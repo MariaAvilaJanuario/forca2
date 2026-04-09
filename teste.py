917abc624868522c4847bf4c12fc81924fa38efc
Duda <maria.avila.januario@escola.pr.gov.br>
2026-04-09 11:56:44 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,34 @@
+import random
+print ("-- JOGO DA FORCA --")
+
+with open("palavras.txt") as f:
+    conteudo = f.read()
+    palavras = conteudo.splitlines()
+    escolhida = random.choice(palavras)
+
+secreta = [ ]
+
+for x in escolhida:
+    secreta.append("_")
+print( )
+for x in secreta:
+    print(x, end = ' ')
+print ( )
+
+y = 0
+while (y < 10):
+    chute = str(input("Digite uma letra: "))
+    if (chute in escolhida):
+        print (chute, " existe na palavra")
+        
+        for i in range(0,len(escolhida)):
+            if escolhida[i] == chute:
+                secreta[i] = chute
+        
+        for x in secreta:
+            print(x, end = ' ')
+        print ( )
+
+    elif (chute not in escolhida):
+        print (chute, " não existe na palavra")
+        y = y + 1
